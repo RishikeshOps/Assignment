755a2602ed71b002e2c906e2b80c9efe60b91a84
Rushikesh Mashidkar <rishikeshmashidkar@gmail.com>
2023-02-25 08:37:53 +0000

    initial commit

--- Task-3/test_website.py
+++ Task-3/test_website.py
@@ -1,17 +1,22 @@
 import requests
 import unittest
 
-class TestWebsiteLoading(unittest.TestCase):
-    
-    def test_website_loading(atg):
+
+class TestATGWorldWebsite(unittest.TestCase):
+
+    def test_website_loading(self):
         # Set up the URL to connect
-        url = "https://atg.world/"
-        
+        url = "https://atg.world"
+        print(f"Connecting to {url}...")
+
         # Connect to the website
         response = requests.get(url)
-        
+        print(f"Response status code: {response.status_code}")
+        print(f"Response content: {response.content[:50]}...")
+
         # Verify the response code
-        atg.assertEqual(response.status_code, 200)
-        
+        self.assertEqual(response.status_code, 200)
+        print("Website loaded successfully!")
+
 if __name__ == '__main__':
     unittest.main()
